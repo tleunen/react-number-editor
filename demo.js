cbc6a3b09c0dc7976d0f895df40d1bff5a2636e1
Cao Võ Thanh Tòng <95293444+CaoVoThanhTong@users.noreply.github.com>
2023-07-19 15:57:59 +0700

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var NumberEditor = require('react-number-editor');
 
+
+
 React.render(
     <NumberEditor min={0} max={1} step={0.01} decimals={2} onValueChange={onValueChange} />,
     document.body
